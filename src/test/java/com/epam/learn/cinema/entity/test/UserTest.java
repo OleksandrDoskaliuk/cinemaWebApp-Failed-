aee829180ab5db8941cb8647ad0b1df7bc0b13d9
OleksandrDoskaliuk <sashadoskaliyk@gmail.com>
2021-05-15 15:57:19 +0300

    Rename field

--- src/test/java/com/epam/learn/cinema/entity/test/UserTest.java
+++ src/test/java/com/epam/learn/cinema/entity/test/UserTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import com.epam.learn.cinema.entities.User;
 
 public class UserTest {
-	private int id = 1;
+	private int userId = 1;
 	private String firstname = "Ivan";
 	private String lastname = "Ivanovich";
 	private String login = "ivanIvanovicn";
@@ -17,34 +17,34 @@ public class UserTest {
 	public void createUserWithoutParamsTest() {
 		
 		User user = new User();
-		user.setId(id);
+		user.setUserId(userId);
 		user.setFirstname(firstname);
 		user.setLastname(lastname);
 		user.setLogin(login);
 		user.setPassword(password);
-		user.setroleId(roleId);
+		user.setRoleId(roleId);
 		
-		Assert.assertEquals(id, user.getId());
+		Assert.assertEquals(userId, user.getUserId());
 		Assert.assertEquals(firstname, user.getFirstname());
 		Assert.assertEquals(lastname, user.getLastname());
 		Assert.assertEquals(login, user.getLogin());
 		Assert.assertEquals(password, user.getPassword());
-		Assert.assertEquals(roleId, user.getroleId());
+		Assert.assertEquals(roleId, user.getRoleId());
 	}
 	
 	@Test
 	public void userHashCodeTest() {
-		User first = User.createUser(id, firstname, lastname, login, password, roleId);
-		User second = User.createUser(id, firstname, lastname, login, password, roleId + 1);
+		User first = User.createUser(userId, firstname, lastname, login, password, roleId);
+		User second = User.createUser(userId, firstname, lastname, login, password, roleId + 1);
 		
 		Assert.assertNotEquals(first.hashCode(), second.hashCode());
 	}
 	
 	@Test
 	public void userEqualsTest() {
-		User first = User.createUser(id, firstname, lastname, login, password, roleId);
-		User userWithSameLogin = User.createUser(id + 1, "Andrii", "Lastivka", login, password, roleId + 1);
-		User userWithDiffLogin = User.createUser(id + 2, "Andrii", "Lastivka", "newLogin", password, roleId + 2);
+		User first = User.createUser(userId, firstname, lastname, login, password, roleId);
+		User userWithSameLogin = User.createUser(userId + 1, "Andrii", "Lastivka", login, password, roleId + 1);
+		User userWithDiffLogin = User.createUser(userId + 2, "Andrii", "Lastivka", "newLogin", password, roleId + 2);
 		
 		Assert.assertEquals(first, first);
 		Assert.assertEquals(first, userWithSameLogin);
@@ -54,8 +54,8 @@ public class UserTest {
 	
 	@Test
 	public void userToStringTest() {
-		User user = User.createUser(id, firstname, lastname, login, password, roleId);
-		String expected = "User [id=1, firstname=Ivan, lastname=Ivanovich, login=ivanIvanovicn, password=123, roleId=1]";
+		User user = User.createUser(userId, firstname, lastname, login, password, roleId);
+		String expected = "User [userId=1, firstname=Ivan, lastname=Ivanovich, login=ivanIvanovicn, password=123, roleId=1]";
 		String actual = user.toString();
 		Assert.assertEquals(expected, actual);
 	}
